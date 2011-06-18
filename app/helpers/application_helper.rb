ee0df412402080aedd0878d45c304f5c1ccbe02b
Gerardo Barcia <gerardobarciap@gmail.com>
2011-06-18 20:12:55 +0200

    Removiendo pagina incial de Rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Definicion del metodo de titulo base para las paginas
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
